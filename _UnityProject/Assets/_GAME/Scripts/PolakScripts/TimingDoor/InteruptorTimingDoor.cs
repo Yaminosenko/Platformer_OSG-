568203162f20012b7d9eb8d8977f6101f996d063
polskonexion69 <48548145+polskonexion69@users.noreply.github.com>
2020-02-07 16:16:50 +0100

    Add TimingDoor _Tableau 3

---
+++ _UnityProject/Assets/_GAME/Scripts/PolakScripts/TimingDoor/InteruptorTimingDoor.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteruptorTimingDoor : MonoBehaviour
+{
+    public TimingDoor _linkedObject;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player" || other.gameObject.tag == "Clone")
+        {
+            _linkedObject.Open();
+        }
+    }
+}
