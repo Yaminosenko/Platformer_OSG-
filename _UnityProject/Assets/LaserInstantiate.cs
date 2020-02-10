95dcbeb7a8eab9943af11efeb88807b9768d2651
LightNaruto <tenn.maurin@gmail.Com>
2020-02-10 13:54:24 +0100

    Add : TestLaser

---
+++ _UnityProject/Assets/LaserInstantiate.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Rewired;
+
+public class LaserInstantiate : InputListener
+{
+    [SerializeField] private Transform _startLaserPosition;
+    [SerializeField] private Vector2 _LaserDirection;
+    private PlayerMouse _myMouseControler;
+
+
+    private void Awake()
+    {
+        _myMouseControler = GetComponent<PlayerMouse>();
+    }
+    void Update()
+    {
+        _LaserDirection = _myMouseControler.screenPosition;
+
+        Debug.Log(_LaserDirection);
+    }
+    public void LaserBehavior()
+    {
+     
+    }
+}
