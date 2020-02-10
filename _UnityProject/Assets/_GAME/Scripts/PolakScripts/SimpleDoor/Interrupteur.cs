b36ee74e1acbe25f4489cdec4c9236f2fcc87617
LightNaruto <tenn.maurin@gmail.Com>
2020-02-11 00:44:27 +0100

    add : ModifRapideInterupt

--- _UnityProject/Assets/_GAME/Scripts/PolakScripts/SimpleDoor/Interrupteur.cs
+++ _UnityProject/Assets/_GAME/Scripts/PolakScripts/SimpleDoor/Interrupteur.cs
@@ -1,36 +1,54 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Rewired;
+
 
 public class Interrupteur : MonoBehaviour
 {
     public SimpleDoor _linkedObject;
     [SerializeField] private int _count = 0;
+    [SerializeField] private GameObject _uxInteractionFeedback;
+
+    private bool _isOntrigger = false;
 
 
-
-    private void OnTriggerEnter(Collider other)
+     private void OnTriggerEnter(Collider other)
     {
-        Debug.Log("hi");
-        if (other.gameObject.layer == 9 || other.gameObject.layer == 13)
+        if (other.gameObject.layer == 9)
         {
-            _count++;
+        _isOntrigger = true;
+        _uxInteractionFeedback.SetActive(true);
+        }
+     
+        if (other.gameObject.layer == 9 || other.gameObject.layer == 13 )
+        {
+         
+                _count++;
 
-            if (_count > 1)
-            {
-                _count = 0;
-            }
+                if (_count > 1)
+                {
+                    _count = 0;
+                }
 
-            if (_count == 1)
-            {
-                _linkedObject.Open();
-            }
+                if (_count == 1)
+                {
+                    _linkedObject.Open();
+                }
 
-
-            if (_count == 0)
-            {
-                _linkedObject.Close();
-            }
+                if (_count == 0)
+                {
+                    _linkedObject.Close();
+                }
+            
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.layer == 9)
+        {
+             _isOntrigger = false;
+            _uxInteractionFeedback.SetActive(false);
         }
     }
 }
