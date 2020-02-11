5fcbae5a05e18023299eaabff46cce99b8a28f70
Dimitry <xxhellfire69xx@gmail.com>
2020-02-11 16:39:58 +0100

    Add: Shoot_le_bébé

---
+++ _UnityProject/Assets/_GAME/Scripts/DimiScripts/BlasterScript.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlasterScript : MonoBehaviour
+{
+    public float speedVelocity;
+
+    private void Update()
+    {
+        transform.Translate(0, 0, speedVelocity, Space.Self);
+    }
+}
