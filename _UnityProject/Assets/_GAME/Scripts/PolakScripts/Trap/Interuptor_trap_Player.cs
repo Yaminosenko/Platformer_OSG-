dcb1ab476ef3e51c963b0ecc817e7e38c253a6c1
polskonexion69 <48548145+polskonexion69@users.noreply.github.com>
2020-02-12 19:30:04 +0100

    Add niveau 3_ trap

---
+++ _UnityProject/Assets/_GAME/Scripts/PolakScripts/Trap/Interuptor_trap_Player.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Interuptor_trap_Player : MonoBehaviour
+{
+    [SerializeField] private GameObject _playerTrap;
+    [SerializeField] private GameObject _cloneTrap;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.layer == 9 || other.gameObject.layer == 13)
+        {
+            _playerTrap.SetActive(true);
+            _cloneTrap.SetActive(false);
+
+        }
+    }
+}
