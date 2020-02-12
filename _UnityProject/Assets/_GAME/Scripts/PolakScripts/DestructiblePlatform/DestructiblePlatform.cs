aef7a0a91a2a79a67469787debafe70c1ca8077f
polskonexion69 <48548145+polskonexion69@users.noreply.github.com>
2020-02-12 21:38:50 +0100

    add movementplatforment_ destructibleplatform

---
+++ _UnityProject/Assets/_GAME/Scripts/PolakScripts/DestructiblePlatform/DestructiblePlatform.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestructiblePlatform : MonoBehaviour
+{
+    [SerializeField] private GameObject platform;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.layer == 9)
+        {
+            StartCoroutine("Timetodestroy");
+        }
+    }
+
+    IEnumerator Timetodestroy()
+    {
+        yield return new WaitForSeconds(1);
+        platform.SetActive(false);
+        StartCoroutine("Timetorespawn");
+    }
+
+    IEnumerator Timetorespawn()
+    {
+        yield return new WaitForSeconds(3);
+        platform.SetActive(true);
+    }
+}
---
+++ _UnityProject/Assets/_GAME/Scripts/PolakScripts/MovingPlatform/MovementPLatform.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementPLatform : MonoBehaviour
+{
+    [SerializeField] private Transform _platform;
+
+    public float _speed;
+
+    [SerializeField] private Transform _pos1;
+    [SerializeField] private Transform _pos2;
+
+    bool pos1 = false;
+   
+    // Start is called before the first frame update
+
+    void Start()
+    {
+        pos1 = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+     
+       if (pos1 == false)
+        {
+            _platform.position = Vector3.MoveTowards(_platform.position, _pos1.position, _speed*Time.deltaTime );
+        }
+
+
+        if (pos1 == true)
+        {
+            _platform.position = Vector3.MoveTowards(_platform.position, _pos2.position, _speed * Time.deltaTime);
+        }
+
+
+
+
+        if (_platform.position == _pos1.position)
+        {
+            pos1 = true;
+        }
+
+
+        if (_platform.position == _pos2.position)
+        {
+            pos1 = false;
+        }
+
+
+    }
+}
