6e487c3c92bb6666e1f0e24c28dafc069fef3ee6
Dimitry <xxhellfire69xx@gmail.com>
2020-02-12 14:28:56 +0100

    Update:Anim_ghost

--- _UnityProject/Assets/_GAME/Scripts/DimiScripts/Test.cs
+++ _UnityProject/Assets/_GAME/Scripts/DimiScripts/Test.cs
@@ -4,36 +4,5 @@ using UnityEngine;
 
 public class Test : MonoBehaviour
 {
-    public Transform _test;
-    private GameObject _yolo;
-    float timer = 0;
-    float recallPeriod = 2.0f;
-    public List<Vector3> positions = new List<Vector3>();
-
-
-
-    private void OnEnable()
-    {
-        Transform _testIns = Instantiate(_test);
-        _test = _testIns;
-    }
-    private void Update()
-    {
-        TrackPositions();
-        _test.position = positions[0];
-    }
-    void TrackPositions()
-    {
-        Debug.Log(positions);
-        if (timer > recallPeriod)
-        {
-            positions.RemoveAt(0);
-            positions.Add(transform.position);
-        }
-        else
-        {
-            positions.Add(transform.position);
-            timer += Time.deltaTime;
-        }
-    }
+    
 }
