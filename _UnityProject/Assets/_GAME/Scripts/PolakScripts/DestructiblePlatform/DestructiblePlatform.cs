b1259e829cc52088a62f8e5eb3a6165a03474779
jeje garceau <jejegarceau26@gmail.com>
2020-02-17 15:42:57 +0100

    Update: Niveau2Final_DestructiblePlateform

--- _UnityProject/Assets/_GAME/Scripts/PolakScripts/DestructiblePlatform/DestructiblePlatform.cs
+++ _UnityProject/Assets/_GAME/Scripts/PolakScripts/DestructiblePlatform/DestructiblePlatform.cs
@@ -4,11 +4,20 @@ using UnityEngine;
 
 public class DestructiblePlatform : MonoBehaviour
 {
-    [SerializeField] private GameObject platform;
+     private GameObject platform;
+     private MeshRenderer mesh;
+    [SerializeField] private GameObject child;
+     private BoxCollider collider;
+    
+    [SerializeField] private float _timeBeforeDestruction = 2;
+    [SerializeField] private float _timeReconstruction = 4;
     // Start is called before the first frame update
     void Start()
     {
-        
+        platform = GetComponent<GameObject>();
+        collider = GetComponentInChildren<BoxCollider>();
+        mesh = GetComponentInChildren<MeshRenderer>();
+        //child = GetComponentInChildren<GameObject>();
     }
 
     // Update is called once per frame
@@ -27,14 +36,20 @@ public class DestructiblePlatform : MonoBehaviour
 
     IEnumerator Timetodestroy()
     {
-        yield return new WaitForSeconds(2);
-        platform.SetActive(false);
+        yield return new WaitForSeconds(_timeBeforeDestruction);
+        collider.enabled = false;
+        //mesh.enabled = false;
+        child.SetActive(false);
+
         StartCoroutine("Timetorespawn");
     }
 
     IEnumerator Timetorespawn()
     {
-        yield return new WaitForSeconds(4);
-        platform.SetActive(true);
+        yield return new WaitForSeconds(_timeReconstruction);
+        child.SetActive(true);
+        collider.enabled = true;
+        //collider.isTrigger = false;
+        //mesh.enabled = true;
     }
 }
