f48f2da9c85705a5400bc9aaf6534f98d0f73641
LightNaruto <tenn.maurin@gmail.Com>
2020-02-19 16:41:00 +0100

    Add: SceneManager

---
+++ _UnityProject/Assets/_GAME/Scripts/DimiScripts/TriggerChangeScene.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TriggerChangeScene : MonoBehaviour
+{
+    public string _sceneName;
+    [SerializeField] private int _indexSceneToLoad;
+    [SerializeField] private GameManager _gameManager;
+
+    private void OnTriggerEnter(Collider col)
+    {
+        if(col.gameObject.layer == 9)
+        {
+            ChangeScene();
+        }
+    }
+
+    public void ChangeScene()
+    {
+        SceneManager.LoadScene(_indexSceneToLoad);
+        if (_gameManager != null)
+        {
+            Destroy(_gameManager.gameObject);
+        }
+    }
+}
