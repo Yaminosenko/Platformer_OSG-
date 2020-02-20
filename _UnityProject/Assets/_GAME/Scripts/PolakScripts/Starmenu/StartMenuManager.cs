3ac4f1c50bf443c0dc5e3dec898c26f559d5445a
polskonexion69 <48548145+polskonexion69@users.noreply.github.com>
2020-02-20 16:16:01 +0100

    add menustart

---
+++ _UnityProject/Assets/_GAME/Scripts/PolakScripts/Starmenu/StartMenuManager.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartMenuManager : MonoBehaviour
+{
+
+    public GameObject _playPanel;
+    public GameObject _controlPanel;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+   public void PlayButton()
+    {
+        SceneManager.LoadScene("Niveau01");
+    }
+
+    public void ControlButton()
+    {
+        _playPanel.SetActive(false);
+        _controlPanel.SetActive(true);
+    }
+
+    public void BackButton()
+    {
+        _playPanel.SetActive(true);
+        _controlPanel.SetActive(false);
+    }
+
+    public void QuitButton()
+    {
+        Application.Quit();
+    }
+
+}
